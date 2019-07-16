b16173836133504f7b50446fbdc78e8680019546
Azores <m_azores91446@tss-hr420-dm17.fanshawec.ca>
2019-07-16 17:49:22 -0400

    dev.ma.addingLightBox

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -5,8 +5,9 @@
 	const sigils = document.querySelectorAll('.sigilContainer'),
 		lightBox = document.querySelector('.lightbox'),
 		closeButton = document.querySelector('.close-lightbox'),
-		houseVideo = document.querySelector('.house-video');
-
+		houseVideo = document.querySelector('.house-video'),
+		bannerImages = document.querySelector('#houseImages');
+	
 	function popLightBox() {
 		//debug this so far and make sure the event handling works
 		//debugger;
@@ -26,7 +27,26 @@
 		houseVideo.pause();
 	}
 
-	sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
+	function animateBanners() {
+		// we need an offset that we can multiply by to animate
+		// our banner to the left and make the active one show up
+		
+		let offset = 600
+			multiplier = this.dataset.offset; //this is the data-offset custom data attribute
+			//on each of the sigils
+		console.log((offset * multiplier) + "px");
+
+		//move banners to left using product of math
+		bannerImages.style.right = `${offset * multiplier + "px"}`;
+
+		//debugger;
+
+	}
+
+	//sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
+	
+	sigils.forEach(sigil => sigil.addEventListener("click", animateBanners));
+
 	closeButton.addEventListener("click", closeLightBox);
 
 	houseVideo.addEventListener('ended', closeLightBox);
